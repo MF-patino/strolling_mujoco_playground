705e09d59fbfb5483a505c6b56c055f20a72f903
MF-patino <m.fpatino@udc.es>
2026-03-01 18:18:20 +0100

    Greedy policy switching algorithm implementation
    We greedily collect evidence of the seemingly best performing policy iteratively. This converges on the probably best policy for the environment after domain change detection.

--- learning/visualize.py
+++ learning/visualize.py
@@ -104,6 +104,7 @@ def load_env(env_name, impl):
 # Work in progress controller for the robot
 class RobotController:
     def __init__(self, obs_shape, act_shape, initial_env=None, jit_inference=None):
+        self.cmd = jp.array([1., 0., 0.])
         self.wms = []
         self.policies = {}
 
@@ -124,17 +125,21 @@ class RobotController:
         # JIT compile the gradient descent logic
         self.fast_update = jax.jit(train_step)
         
-        # History for domain detection
-        self.errors = []
+        # History of errors for plotting 
+        self.errors = {name: [] for name, _, _ in self.wms}
         self.drift_indices = []
 
         # total_size=1000: Takes 20 seconds to fill the queue
         # window_size=250: Detect changes based on the last 5 seconds of data
         window_size = 100
-        self.detector = KSDriftDetector(total_size=1000, window_size=window_size, adwin_delta=1e-2)
+        self.detector = KSDriftDetector(total_size=1000, window_size=window_size, adwin_delta=5e-2)
         self.buffer = deque(maxlen=window_size)
+        
+        self.to_sample = None
 
     def loadWorldModels(self, initial_env):
+        self.wm_dict = {}
+
         for env_name in ALL_ENVS:
             root = f"world_models/{env_name}/"
 
@@ -151,6 +156,7 @@ class RobotController:
             rng = jax.random.PRNGKey(0)
             wm_state = create_train_state(rng, learning_rate=1e-5, sensor_dim=sensor_dim, action_dim=action_dim) # Low LR for stability
             self.wms.append((env_name, wm_state.replace(params=params), stats))
+            self.wm_dict[env_name] = (env_name, wm_state.replace(params=params), stats)
 
             # The active world model is the one corresponding to the environment we launch the robot in
             if env_name == initial_env:
@@ -213,9 +219,19 @@ class RobotController:
         act_arr = jp.stack(batch_act)
         next_arr = jp.stack(batch_next)
         
-        #return jp.mean((self.getPredictionWM(wm, stats, obs_arr, act_arr) - next_arr) ** 2)
-        statistic, _ = ks_2samp(next_arr.reshape(-1), self.getPredictionWM(wm, stats, obs_arr, act_arr).reshape(-1))
-        return statistic
+        return jp.mean((self.getPredictionWM(wm, stats, obs_arr, act_arr) - next_arr) ** 2)
+
+    def getErrorWM(self, name, wm, stats, obs, action, next_obs):
+        pred_next_obs = self.getPredictionWM(wm, stats, obs, action)
+        
+        # Calculate error
+        error = jp.mean((pred_next_obs - next_obs) ** 2)
+        alpha = 0.1
+        smooth_error = error if len(self.errors[name]) == 0 else alpha * error + (1-alpha) * self.errors[name][-1]
+        
+        self.errors[name].append(smooth_error)
+
+        return error
 
     def control_loop(self, obs, action, next_obs):
         if not self.deploy:
@@ -225,22 +241,59 @@ class RobotController:
         
         self.buffer.append((obs, action, next_obs))
 
+        # TODO: implement Multi-Armed Bandit with policy embeddings
+        if self.to_sample is not None:
+            prev_name = self.active_wm[0]
+            self.active_wm = None
+            for try_wm in self.to_sample:
+                name = try_wm[0]
+                sample_num = len(self.sampled_errors[name])
+                if sample_num < self.samples2collect[name]:
+                    if prev_name != name:
+                        print(f"Sampling {name}.")
+                    self.active_wm = try_wm
+                    self.inference = self.policies[name]
+                    break
+
+            if self.active_wm is None:
+                # Determine the policy that garnered more success (lowest mean WM error)
+                errs = [(jp.mean(jp.array(self.sampled_errors[name])), name) for name in self.sampled_errors]
+                best_err, best_name = min(errs)
+
+                if self.iteration < self.max_iterations:
+                    # Update rule: collect more samples of a policy that improves WM error (to stabilize the robot)
+                    # Also, don't collect more samples of policies that increase WM error (as it destabilizes the robot)
+                    
+                    # We collect more samples of a promising policy to give us more evidence in favour or to the contrary
+                    # that the policy is actually better than the rest
+                    delta = lambda err: 15 if err == best_err else 0
+                    self.samples2collect = {name: int(self.samples2collect[name] + delta(err)) for err, name in errs}
+                    
+                    self.active_wm = [(name, wm, stats) for name, wm, stats in self.to_sample if len(self.sampled_errors[name]) < self.samples2collect[name]][0]
+                    next_name = self.active_wm[0]
+                    self.inference = self.policies[next_name]
+                    self.iteration += 1
+
+                    print(f"Beginning iteration {self.iteration} with {next_name}.")
+                else:
+                    print(f"Converged on {best_name}.")
+                    self.active_wm = self.wm_dict[best_name]
+                    self.inference = self.policies[best_name]
+                    self.to_sample = None
+
         active_name, wm, stats = self.active_wm
-        pred_next_obs = self.getPredictionWM(wm, stats, obs, action)
-        
-        # Calculate error
-        error = jp.mean((pred_next_obs - next_obs) ** 2)
-        self.errors.append(error)
+        error = self.getErrorWM(active_name, wm, stats, obs, action, next_obs)
+        #[self.getErrorWM(name, wm, stats, obs, action, next_obs) for name, wm, stats in self.wms if name != active_name]
+
+        if self.to_sample is not None:
+            self.sampled_errors[active_name].append(error)
+            return
         
         # Update detector
         is_drift, statistic = self.detector.update(error)
 
         step = len(self.detector.stat_values)
         
-        #if step % 100 == 0:
-        #    wm_info = [(self.getMetricWM(wm, stats), name, wm, stats) for name, wm, stats in self.wms] #if name != active_name]
-        #    print([(error, name) for error, name, _, _ in wm_info])
-
         if is_drift:
             idx = step - 1
             self.drift_indices.append(idx)
@@ -248,16 +301,36 @@ class RobotController:
 
             wm_info = [(self.getMetricWM(wm, stats), name, wm, stats) for name, wm, stats in self.wms if name != active_name]
             #print([(error, name) for error, name, _, _ in wm_info])
-            best = min(wm_info)
-            _, best_name, best_wm, best_stats = best
 
-            self.active_wm = (best_name, best_wm, best_stats)
-            self.inference = self.policies[best_name]
-            print(f"Continuing with {best_name}.")
+            # We sort by mean WM error (this implicitly sorts the policies in relation to their similarity
+            # with the active policy)
+            self.to_sample = [self.active_wm] + [(name, wm, stats) for _, name, wm, stats in sorted(wm_info)]
+            self.sampled_errors = {name: [] for name, _, _ in self.to_sample}
+
+            # All policies are safely sampled 15 times (.3 seconds) initially. 
+            # 20 (.4 seconds) is too much for slippery in rough terrain: robot falls.
+            self.initial_samples = 15
+
+            # Assign last obtained errors so that we may be able to compare other policies
+            # against the originally active one without having to roll it out
+            self.sampled_errors[active_name] = self.errors[active_name][-self.initial_samples:]
+
+            # When checking out different policies, the amount of samples to collect of each
+            self.samples2collect = {name: self.initial_samples for name, _, _ in self.to_sample}
+            self.iteration = 1
+            # Should converge after 5 iterations
+            self.max_iterations = 5
 
         if is_drift and step > self.detector.min_samples:
-            #plt.hist(self.errors, bins=25, range=(0, 2))
-            #plt.title("Error distribution on flat ground (Zoomed 0-2)")
+            '''for wm_name in self.errors:
+                plt.plot(self.errors[wm_name], label=f"{wm_name} WM errors")
+
+            plt.xlabel("Time step")
+            plt.title("WM error history")
+            plt.legend()
+            plt.tight_layout()
+            plt.show()'''
+
             plt.plot(self.detector.stat_values, label="KS statistic")
 
             plt.vlines(self.drift_indices,
@@ -329,7 +402,7 @@ def interactive_visualization(env, controller=None, resetNum=-1, jit_inference=N
                     break
 
             # Instruct robot to go always forwards
-            state.info["command"] = jp.array([1., 0., 0.])
+            state.info["command"] = controller.cmd
 
             step_start = time.time()
             rng, act_rng = jax.random.split(rng)
@@ -378,18 +451,19 @@ def main():
     slippery_env = load_env("Go2StrollSlipperyTerrain", IMPL)
 
     # TODO:
-    # Cambio entre 3 WMs diferentes (top k + probarlos)
+    # 
     # Entrenar rugoso desde llano
     # Comprobar que funciona en el G1
 
     obs_shape, act_shape = env.observation_size, env.action_size
 
-    controller = RobotController(obs_shape, act_shape, initial_env="Go2StrollFlatTerrain")
+    controller = RobotController(obs_shape, act_shape, initial_env=env_name)
 
+    interactive_visualization(env, controller=controller, resetNum=1)
+    interactive_visualization(rough_env, controller=controller, resetNum=1)
     interactive_visualization(env, controller=controller, resetNum=1)
     interactive_visualization(slippery_env, controller=controller, resetNum=1)
     interactive_visualization(env, controller=controller, resetNum=1)
-    interactive_visualization(rough_env, controller=controller, resetNum=1)
 
 if __name__ == "__main__":
     main()
