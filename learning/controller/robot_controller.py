GP search guided by WM error again, fixed normalization issues and tweaked GP kernel

learning/controller/robot_controller.py
@@ -17,7 +17,7 @@ from controller.ks_detector import KSDriftDetector
 import controller.plots as plots
 
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
 from sklearn.decomposition import TruncatedSVD
 IMPL = "jax"
 
@@ -46,15 +46,14 @@ class RobotController:
 
         if jit_inference is not None:
             self.inference = jit_inference
-        
+
         if not self.deploy:
             return
-        
+
         self.env_names = os.listdir(MODELS_ROOT)
-        
-        # Histories for plotting 
-        self.errors = {name: [] for name in self.env_names}
-        self.rewards = {name: [] for name in self.env_names}
+
+        # Histories for plotting
+        self.smooth_errors = {name: [] for name in self.env_names}
         self.env_changes = []
         self.drift_indices = []
         self.contact_history = []
@@ -65,17 +64,18 @@ class RobotController:
         # total_size=1000: 20 seconds of total memory
         # window_size=100: Detect changes based on the last 2 seconds of data
         self.detector = KSDriftDetector(total_size=1000, window_size=100, adwin_delta=.1)
-        
+
         # GP search parameters:
+        self.errors = {name: [] for name in self.env_names}
         # Should converge after 5 iterations (empirically tested)
         self.max_iterations = 5
         self.sampling = False
         # Policy embeddings will have a maximum of 4 dimensions for the GP
         self.max_pol_emb_dim = 4
-        # Policies are safely sampled 25 times (.5 seconds) each iteration. 
+        # Policies are safely sampled 25 times (.5 seconds) each iteration.
         # Switching policies at a higher rate can cause stability issues.
         self.increment_samples = 25
-        # After sampling, we discard the first 10 samples of each iteration when
+        # After sampling, we discard the first 15 samples of each iteration when
         # fitting the GP. This is done to disregard the noise when switching policies
         self.noisy_samples = 15
         # If the final policy selected is different from the currently
@@ -90,7 +90,7 @@ class RobotController:
         self.native_errors = {}
         self.inaffinity_matrix = jp.stack([self.computePolicyEmbedding(name) for name in self.env_names])
         print(f"Raw inaffinity matrix:\n{self.inaffinity_matrix}")
-        
+
         if len(self.env_names) > 1:
             self.normalizePolicyEmbeddings()
 
@@ -102,13 +102,13 @@ class RobotController:
             "inaffinity_matrix": self.inaffinity_matrix,
             "env_names": self.env_names,
             "detector": self.detector,
-            "errors": self.errors,
+            "smooth_errors": self.smooth_errors,
             "policy_history": self.policy_history,
             "contact_history": self.contact_history,
             "drift_indices": self.drift_indices,
             "policy_embeddings": self.policy_embeddings,
             "env_changes": self.env_changes,
-            "gp_states": self.gp_states 
+            "gp_states": self.gp_states
         }
         with open(path, 'wb') as f:
             pickle.dump(data, f)
@@ -126,10 +126,10 @@ class RobotController:
             # Extract the top 4 principal components that explain the most variance
             reducer = TruncatedSVD(n_components=self.max_pol_emb_dim)
             inaffinity_matrix = reducer.fit_transform(self.inaffinity_matrix)
-            
+
             # Convert back to JAX array for the rest of the pipeline
             inaffinity_matrix = jp.array(inaffinity_matrix)
-            
+
             # Optional: Print how much of the original information was preserved
             variance_ratio = sum(reducer.explained_variance_ratio_) * 100
             print(f"SVD preserved {variance_ratio:.2f}% of the variance.")
@@ -139,7 +139,7 @@ class RobotController:
         # Normalization to get final embeddings.
         # After this step, we will be able to measure the cosine distance between them to get the "semantic"
         # difference between two different policies.
-        norm_mat = inaffinity_matrix / jp.linalg.norm(inaffinity_matrix, axis=1, keepdims=True) 
+        norm_mat = inaffinity_matrix / jp.linalg.norm(inaffinity_matrix, axis=1, keepdims=True)
 
         self.policy_embeddings = {
             name: embedding for name, embedding in zip(self.env_names, norm_mat)
@@ -158,7 +158,7 @@ class RobotController:
                 params = pickle.load(f)
             with open(WM_STATS_PATH.format(env_name=env_name), 'rb') as f:
                 stats = pickle.load(f)
-                
+
             sensor_dim = stats['obs_mean'].shape[0]
             action_dim = stats['act_mean'].shape[0]
 
@@ -196,7 +196,7 @@ class RobotController:
                 )
             else:
                 network_factory = network_fn
-            
+
             ppo_network = network_factory(
                 obs_shape, act_shape, preprocess_observations_fn=normalize
             )
@@ -211,21 +211,22 @@ class RobotController:
 
         # The active policy is the one corresponding to the environment we launch the robot in
         self.inference = self.policies[initial_env]
-    
-    
+
+
     def getPredictionWM(self, wm, stats, obs, action):
         # Predict what should have happened
         return _jit_predict_wm(wm.apply_fn, wm.params, stats, obs, action)
 
     def getErrorWM(self, name, wm, stats, obs, action, next_obs):
         pred_next_obs = self.getPredictionWM(wm, stats, obs, action)
-        
+
         # Calculate error
         error = jp.linalg.norm(pred_next_obs - next_obs)
         alpha = 0.1
         smooth_error = error if len(self.errors[name]) == 0 else alpha * error + (1-alpha) * self.errors[name][-1]
-        
-        self.errors[name].append(smooth_error)
+
+        self.errors[name].append(error)
+        self.smooth_errors[name].append(smooth_error)
 
         return error
 
@@ -244,22 +245,22 @@ class RobotController:
             for _, wm, stats in self.wms
         ])
         mean_errors = jp.mean(errors, axis=1)
-        
+
         env_index = self.env_names.index(env_name)
         # Compute the extra "surprise" by subtracting the baseline noise
         baseline_error = mean_errors[env_index]
         embedding = jp.abs(mean_errors - baseline_error)
 
         self.native_errors[env_name] = (baseline_error, errors[env_index,:])
-        
+
         return embedding
-        
+
     def predictPolicyScore(self, name, active_name):
-        # GP predicts the expected reward (negative error) and uncertainty
+        # GP predicts the expected error and uncertainty
         pol_emb = self.policy_embeddings[name]
         active_emb = self.policy_embeddings[active_name]
         mean, std = self.gp.predict([pol_emb], return_std=True)
-                        
+
         # UCB formula: Exploit (mean) + Explore (Kappa * std) * Safety constraints
 
         # It is dangerous to rollout policies that are too different together for stability reasons.
@@ -267,40 +268,51 @@ class RobotController:
         # policies have little in common.
         cos_dist = 1 - jp.dot(pol_emb, active_emb) / (jp.linalg.norm(pol_emb) * jp.linalg.norm(active_emb))
         kappa = 2.; gamma = .5
-        ucb_score = mean[0] + kappa * std[0] * jp.exp(-gamma * cos_dist)
+        ucb_score = -mean[0] + kappa * std[0] * jp.exp(-gamma * cos_dist)
         return ucb_score, mean, std
-    
+
     def getFinalPolicyScore(self, pol_name):
         '''
-        Computes the expected reward from the policy's obtained rewards.
+        Computes the expected error from the policy's obtained errors.
         We use only the real data obtained for the final decision after a
         policy search process.
         '''
         # If no data was collected for this policy, the GP model did not
         # think it would be promissing. Donnot choose it.
-        if len(self.sampled_rewards[pol_name]) == 0:
+        if len(self.sampled_errors[pol_name]) == 0:
             return -jp.inf
-        
-        samples = jp.array(self.sampled_rewards[pol_name])
+
+        samples = jp.array(self.sampled_errors[pol_name])
 
         remainder = len(samples) % self.increment_samples
         first_samples = samples[:remainder]
         samples = samples[remainder:].reshape([-1, self.increment_samples])[:,self.noisy_samples:].ravel()
         samples = jp.append(first_samples, samples)
 
-        # Reward consistency by only taking the mean reward into account
-        return jp.mean(samples)
-    
+        # Reward consistency by looking at the mean
+        return -jp.mean(samples)
+
     def getNextPolicy(self, active_name):
         # Fit the GP with the new real-world data
         noise = 1e-06
+        y_var = float(jp.var(self.y_train))
+        
+        # 2. Prevent division by zero in Iteration 1 (where variance is 0.0)
+        # When y_var is 0, scikit-learn doesn't scale the data, so we divide by 1.0
+        y_var_safe = 1.0 if y_var == 0.0 else y_var
+        
+        # 3. NORMALIZE ALPHA! (Divide raw variance by the dataset variance)
+        normalized_alpha = (self.alpha + noise) / y_var_safe
+        max_dist = jp.sqrt(min(len(self.policies), self.max_pol_emb_dim))
+        prior_variance = float(max(1.0, jp.max(normalized_alpha) * 2.0))
+        kernel = C(prior_variance, constant_value_bounds="fixed") * RBF(length_scale=max_dist/2, length_scale_bounds=(1e-2, max_dist))
 
-        self.gp = GaussianProcessRegressor(kernel=RBF(), normalize_y=True, alpha=self.alpha+noise)
+        self.gp = GaussianProcessRegressor(kernel=kernel, normalize_y=True, alpha=normalized_alpha)
         self.gp.fit(self.X_train, self.y_train)
 
         polInfo = [self.predictPolicyScore(name, active_name) + (name,) for name in self.policies]
         best_ucb, mean, std, next_name = max(polInfo)
-        
+
         loggingInfo = [self.iteration, active_name, next_name, polInfo]
         if self.iteration == 1:
             self.gp_states.append([loggingInfo])
@@ -312,23 +324,22 @@ class RobotController:
     def adapt_policy(self, base_policy_name):
         pass
         #raise Exception("Method implemented in OfflineRobotController")
-    
+
     def set_policy(self, pol_name):
         self.active_wm = self.wm_dict[pol_name]
         self.inference = self.policies[pol_name]
-    
+
     def control_loop(self, obs, action, state):
         if not self.deploy:
             return
-        
-        next_obs, reward = state.obs, state.reward
-        obs, next_obs = obs["wm_state"], next_obs["wm_state"]
+
+        obs, next_obs = obs["wm_state"], state.obs["wm_state"]
 
         if self.sampling:
             prev_active_name = self.active_wm[0]
             self.active_wm = None
             for name in self.samples2collect:
-                sample_num = len(self.sampled_rewards[name])
+                sample_num = len(self.sampled_errors[name])
                 if sample_num < self.samples2collect[name]:
                     if prev_active_name != name:
                         print(f"Sampling {name}.")
@@ -348,7 +359,7 @@ class RobotController:
 
                     # Update rule: collect more samples of a policy that improves WM error (to stabilize the robot)
                     # Also, don't collect more samples of policies that increase WM error (as it destabilizes the robot)
-                    
+
                     # We collect more samples of a promising policy to give us more evidence in favour or to the contrary
                     # that the policy is actually better than the rest
                     self.samples2collect[next_name] = self.samples2collect.get(next_name, 0) + self.increment_samples
@@ -363,7 +374,7 @@ class RobotController:
                     # best performing policy, since the search is based on UCB scores.
 
                     # Thus, we base the final choice on scores computed only from the real data collected.
-                    errs = [(self.getFinalPolicyScore(pol_name), pol_name) for pol_name in self.sampled_rewards]
+                    errs = [(self.getFinalPolicyScore(pol_name), pol_name) for pol_name in self.sampled_errors]
                     _, next_name = max(errs)
 
                     print(f"Converged on {next_name}.")
@@ -375,21 +386,24 @@ class RobotController:
                         self.samples2collect[next_name] += self.extra_timesteps
                     else:
                         self.sampling = False
-                        
+
                         if self.generatePlots:
                             plots.plotGaitPattern(self)
                             plots.plotGPSearch(self)
                 else: # End of extra stability iteration
                     self.set_policy(prev_active_name)
                     self.sampling = False
-                    
+
                     if self.generatePlots:
                         plots.plotGaitPattern(self)
                         plots.plotGPSearch(self)
 
         active_name, wm, stats = self.active_wm
+        error = self.getErrorWM(active_name, wm, stats, obs, action, next_obs)
+        # Only needed for plotting WM error of all WMs
+        [self.getErrorWM(name, wm, stats, obs, action, next_obs) for name, wm, stats in self.wms if name != active_name]
+            
         # Fill out histories for plotting
-        self.rewards[active_name].append(reward)
         self.contact_history.append(state.info["last_contact"])
         self.policy_history.append(active_name)
 
@@ -397,11 +411,11 @@ class RobotController:
             # Needed for plotting, we append the 0 manually as we don't want to feed the dummy 0
             # into the KS-ADWIN detection pipeline
             self.detector.stat_values.append(0)
-            self.sampled_rewards[active_name].append(reward)
+            self.sampled_errors[active_name].append(error)
 
             # When sampling batch is over for the policy, compute its new GP datapoint
-            if len(self.sampled_rewards[active_name]) == self.samples2collect[active_name]:
-                samples = jp.array(self.sampled_rewards[active_name])
+            if len(self.sampled_errors[active_name]) == self.samples2collect[active_name]:
+                samples = jp.array(self.sampled_errors[active_name])
                 remainder = len(samples) % self.increment_samples
                 first_samples = samples[:remainder]
                 samples = samples[remainder:].reshape([-1, self.increment_samples])[:,self.noisy_samples:].ravel()
@@ -412,20 +426,16 @@ class RobotController:
                 self.alpha = np.append(self.alpha, jp.var(samples)/len(samples))
 
             return
-        
-        error = self.getErrorWM(active_name, wm, stats, obs, action, next_obs)
-        # Only needed for plotting WM error of all WMs
-        [self.getErrorWM(name, wm, stats, obs, action, next_obs) for name, wm, stats in self.wms if name != active_name]
 
         # Update detector
         is_drift, _, policy_performance_alert = self.detector.update(error, self.native_errors[active_name])
-        
+
         if is_drift or policy_performance_alert:
             if policy_performance_alert:
                 print(f"Policy performance alert")
             else:
                 print(f"Domain change detected")
-            
+
             # The only case where a performance alert is not a valid drift detection
             # happens when the actual drift detection module still doesn't have enough samples yet.
             # If this happens, it must be that a policy search process has just finished and the selected
@@ -436,17 +446,17 @@ class RobotController:
             if len(self.env_names) == 1 or adapt:
                 self.adapt_policy(active_name)
                 return
-            
-            self.sampled_rewards = {name: [] for name, _, _ in self.wms}
+
+            self.sampled_errors = {name: [] for name, _, _ in self.wms}
 
             # We will "seed" the GP search process with some initial samples.
             # IF performance alert: sample only 2 current errors as sometimes these alerts are so quick that 2
             # consecutive, very big error samples are enough signal for it to fire.
             # IF not performance alert: we can assign last obtained errors so that we may start the search process
             # with a bit more useful data.
-            init_samples = 2 if policy_performance_alert else self.increment_samples*2
-            samples = self.rewards[active_name][-init_samples:]
-            self.sampled_rewards[active_name] = samples
+            init_samples = 2 if policy_performance_alert else self.increment_samples
+            samples = self.errors[active_name][-init_samples:]
+            self.sampled_errors[active_name] = samples
 
             self.X_train = np.array([self.policy_embeddings[active_name]])
             samples = jp.array(samples)
@@ -455,11 +465,11 @@ class RobotController:
 
             self.iteration = 1
 
+            self.samples2collect = {active_name: len(samples)}
             next_name = self.getNextPolicy(active_name)
 
-            # When checking out different policies, the amount of samples to collect of each
-            self.samples2collect = {next_name: self.increment_samples}
-            self.samples2collect[active_name] = len(samples)
+            # When rolling out different policies, the amount of samples to collect of each
+            self.samples2collect[next_name] = self.increment_samples
 
             self.sampling = True
 